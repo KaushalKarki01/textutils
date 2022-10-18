a7569316aa20a21abb16f28a515bbfc8a87e2bc0
cowsal <72393915+cowsal@users.noreply.github.com>
2022-10-18 17:21:35 +0545

    Updates

---
+++ precache-manifest.48a87c244d5ee8f2df15ee29cfdcfd4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03bcbd8b6dad4e0af38d",
+    "url": "/textutils/static/css/main.5787cd3f.chunk.css"
+  },
+  {
+    "revision": "03bcbd8b6dad4e0af38d",
+    "url": "/textutils/static/js/main.03bcbd8b.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "f07e92b36f7c3073a0552138c1f5030f",
+    "url": "/textutils/index.html"
+  }
+];
